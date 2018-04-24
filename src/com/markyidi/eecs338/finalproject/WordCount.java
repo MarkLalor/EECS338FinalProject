b7679ad8fb647e28ea4f6b09bdb13d446878cef8
Yidi <huangy6@yahoo.com>
2018-04-23 23:28:26 -0400

    Finish word count

--- src/com/markyidi/eecs338/finalproject/WordCount.java
+++ src/com/markyidi/eecs338/finalproject/WordCount.java
@@ -1,10 +1,36 @@
 package com.markyidi.eecs338.finalproject;
 
-import java.util.Collection;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WordCount {
     public static void main(String[] args) {
+        String dataDirectory = args[0];
+        File ddFile = new File(dataDirectory);
+        List<Path> files = Arrays.stream(ddFile.listFiles())
+                .filter(File::isFile)
+                .map(File::toPath)
+                .collect(Collectors.toList());
 
+        Collection<Map.Entry<String, Integer>> results = MapReduce.mapReduce(files, WordCount::docWordCount, WordCount::reduce);
+    }
+
+    public static Collection<Map.Entry<String, Integer>> docWordCount(Path item) {
+        try (Stream<String> s = Files.lines(item)
+                .flatMap(Pattern.compile("\\s+")::splitAsStream)) {
+            return s
+                    .map(x -> new AbstractMap.SimpleEntry<>(x, 1))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Shoot.");
+        }
     }
 
     public static Integer reduce(Collection<Integer> shuffled) {
