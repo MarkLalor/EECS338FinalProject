e7f9fd1d76200dda4eed82339acb4f42a27c3624
Mark Lalor <markwlalor@gmail.com>
2018-04-30 20:04:02 -0400

    Add shuffle

--- src/com/markyidi/eecs338/finalproject/MapReduce.java
+++ src/com/markyidi/eecs338/finalproject/MapReduce.java
@@ -2,24 +2,39 @@ package com.markyidi.eecs338.finalproject;
 
 import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class MapReduce {
-    public static <T, K, V> Collection<Map.Entry<K, V>> mapReduce(final Collection<T> input, final MRMapper<T, K, V> mapper, final MRReducer<V> reducer) {
-        final Collection<Collection<Map.Entry<K, V>>> mapped = input.stream().map(mapper::map).collect(Collectors.toList());
+    public static <T, K, V> Map<K, V> mapReduce(final Collection<T> input, final MRMapper<T, K, V> mapper, final MRReducer<V> reducer) {
+        final Collection<Collection<Map.Entry<K, V>>> mapped = input.parallelStream()
+                .map(mapper::map)
+                .collect(Collectors.toList());
 
         final Map<K, Collection<V>> shuffled = MapReduce.shuffle(mapped);
 
-        return shuffled.entrySet()
-                .stream()
-                .map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey(), reducer.reduce(entry.getValue())))
-                .collect(Collectors.toList());
+        return shuffled.entrySet().parallelStream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey(),
+                        entry -> reducer.reduce(entry.getValue())));
+    }
+
+    private static <K, V> void addToMultimap(Map<K, Collection<V>> multimap, Map.Entry<K, V> entry) {
+        if (multimap.containsKey(entry.getKey())) {
+            multimap.put(entry.getKey(), new LinkedList<>());
+        }
+
+        multimap.get(entry.getKey()).add(entry.getValue());
     }
 
     private static <K, V> Map<K, Collection<V>> shuffle(Collection<Collection<Map.Entry<K, V>>> mapped) {
         final Map<K, Collection<V>> shuffleMap = new ConcurrentHashMap<>();
+
+        mapped.stream()
+                .flatMap(Collection::stream)
+                .forEach(entry -> addToMultimap(shuffleMap, entry));
 
         return shuffleMap;
     }
